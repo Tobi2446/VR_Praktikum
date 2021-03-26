bd6d47d7bbe02a83627c8c887fd984c5b9b9ca5e
tom2446 <mohr.tobi@gmx.de>
2021-03-26 16:30:11 +0100

    A2

---
+++ Prakikum02/Praktikum02/Assets/Scripts/SkriptFly.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkriptFly : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)) { 
+            this.transform.Translate (Vector3.forward* speed * Time.deltaTime); 
+            }
+        if(Input.GetKey(KeyCode.A)) { 
+            this.transform.Rotate (new Vector3(0,-1,0) * speed * Time.deltaTime);
+             }
+        if(Input.GetKey(KeyCode.S)) { 
+            this.transform.Translate (-1* Vector3.forward * speed * Time.deltaTime); 
+            }
+        if(Input.GetKey(KeyCode.D)) { 
+            this.transform.Rotate (new Vector3(0,1,0) * speed * Time.deltaTime); 
+            }
+        if(Input.GetKey(KeyCode.Q)) { 
+            this.transform.Translate (Vector3.down * speed * Time.deltaTime);
+            }
+        if(Input.GetKey(KeyCode.E)) { 
+            this.transform.Translate (Vector3.up * speed * Time.deltaTime); 
+            }
+    }
+}
---
+++ Prakikum02/Praktikum02/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SwitchScene : MonoBehaviour
+{
+    public string scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.rotation.y > 0.4){
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
