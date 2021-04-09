7f235d9b8d310fdbde877df704d630d5dd76e4bd
tom2446 <mohr.tobi@gmx.de>
2021-04-09 15:12:46 +0200

    A3 Zwischenspeicher

---
+++ Prakikum02/Praktikum02/Assets/Scripts/DisplayFlightData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayFlightData : MonoBehaviour
+{
+    public Text textSpeed;
+    public Text textHeight;
+    public GameObject player;
+    public float warningHeight;
+    public GameObject warnImage;
+    private FlyJoystick flyJoystick;
+    private int wert;
+    private int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flyJoystick = player.GetComponent<FlyJoystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wert = (int) player.transform.position.y;
+        textHeight.text = "Alt: " + wert + "m";
+
+        speed = (int) flyJoystick.speed;
+        int speedAmplifier = (int) flyJoystick.speedAmplifier;
+        textSpeed.text = "Speed: " + speed*speedAmplifier + "km/h";
+
+        if (wert < warningHeight)
+        warnImage.SetActive(true);
+        else 
+        warnImage.SetActive(false);
+
+    }
+}
---
+++ Prakikum02/Praktikum02/Assets/Scripts/FlyJoystick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyJoystick : MonoBehaviour
+{
+    public float speed;
+    public float speedAmplifier;
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputPitch = Input.GetAxis("Pitch") ;
+        float inputYawn = Input.GetAxis("Yawn") * Time.deltaTime * rotationSpeed;
+        speed = Input.GetAxis("Throttle");
+        float inputRoll = Input.GetAxis("Roll");
+
+        this.transform.Rotate(new Vector3(-1,0,0) * inputPitch * Time.deltaTime * rotationSpeed);
+        this.transform.Rotate(new Vector3(0,1,0) * inputRoll * Time.deltaTime * rotationSpeed);
+        this.transform.Rotate(new Vector3(0,0,1) * inputYawn);
+
+        this.transform.Translate(Vector3.forward *speed * Time.deltaTime * speedAmplifier); 
+        
+    }
+}
