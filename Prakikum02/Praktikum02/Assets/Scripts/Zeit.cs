0967688a7d80a9fcae3425fc29c3256d4322397b
tom2446 <mohr.tobi@gmx.de>
2021-04-16 16:53:08 +0200

    Finale Abgabe

---
+++ Prakikum02/Praktikum02/Assets/Scripts/Zeit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Zeit : MonoBehaviour
+{
+    private Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+        InvokeRepeating("TimerUpdate", 0,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerText.text = "Flugzeit: " + Mathf.Round(Time.time);
+    }
+}
